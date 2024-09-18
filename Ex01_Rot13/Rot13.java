3ace77104ab97e56102be3bd24af2b2f52b4ca63
Cèlia Casanovas <2023_celia.casanovas@iticbcn.cat>
2024-09-18 20:54:52 +0200

    Added comments to Rot13.java - currently non functional

--- Ex01_Rot13/Rot13.java
+++ Ex01_Rot13/Rot13.java
@@ -8,21 +8,22 @@ public static class Rot13 {
 
     int findIndex(char inputChar, char[] inputArray) {
         return IntStream.range(0, inputArray.length)
-            .filter(i -> inputChar == inputArray[i])
-            .findFirst()
-            .orElse(-1);
+                .filter(i -> inputChar == inputArray[i])
+                .findFirst()
+                .orElse(-1);
     }
 
     // vull reescriure-ho amb Streams
     String xifraRot13(String inputText) {
         StringBuffer workingStringBuffer = newStringBuffer();
-        for(int i = 0; i < inputText.length(); i++){
+        for (int i = 0; i < inputText.length(); i++) {
             char currentCharacter = inputText.charAt(i);
             int currentAlphabetIndex = findIndex(currentCharacter, LOWERCASE);
+            // s'ha de moure dins de l'if perquè si findIndex retorna -1 el resultat és erroni
             int transformedAlphabetIndex = (currentAlphabetIndex + 13) % LOWERCASE.length;
             if (currentAlphabetIndex > 0) {
                 workingStringBuffer.append(LOWERCASE(currentAlphabetIndex));
-            } else { // només funciona si tots dos arrays són iguals de llargs
+            } else { // només funciona si tots dos arrays són igual de llargs
                 workingStringBuffer.append(UPPERCASE(currentAlphabetIndex));
             }
         }
@@ -35,6 +36,9 @@ public static class Rot13 {
         for (int i = 0; i < encryptedText.length(); i++) {
             char currentCharacter = inputText.charAt(i);
             int currentAlphabetIndex = findIndex(currentCharacter, LOWERCASE);
+            if (currentAlphabetIndex > 0) {
+
+            }
         }
     }
 
