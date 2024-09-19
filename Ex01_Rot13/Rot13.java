8d821fd7cb42c39e4a028847819cd5cab2edf4af
Cèlia Casanovas <2023_celia.casanovas@iticbcn.cat>
2024-09-19 03:03:08 +0200

    Rewrote using streams. Implemented full exercise.

--- Ex01_Rot13/Rot13.java
+++ Ex01_Rot13/Rot13.java
@@ -1,48 +1,95 @@
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-public static class Rot13 {
+public class Rot13 {
     public static final char[] LOWERCASE = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'm', 'n', 'o',
             'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'y', 'z' };
     public static final char[] UPPERCASE = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'M', 'N', 'O',
             'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'X', 'Y', 'Z' };
 
-    int findIndex(char inputChar, char[] inputArray) {
+    private static int findIndex(char inputChar, char[] inputArray) {
         return IntStream.range(0, inputArray.length)
                 .filter(i -> inputChar == inputArray[i])
                 .findFirst()
-                .orElse(-1);
+                .orElse(-1); // mètode d'OptionalInt -> o és un int o és un null, i si és null fem això
     }
 
-    // vull reescriure-ho amb Streams
-    String xifraRot13(String inputText) {
-        StringBuffer workingStringBuffer = newStringBuffer();
-        for (int i = 0; i < inputText.length(); i++) {
-            char currentCharacter = inputText.charAt(i);
-            int currentAlphabetIndex = findIndex(currentCharacter, LOWERCASE);
-            // s'ha de moure dins de l'if perquè si findIndex retorna -1 el resultat és erroni
-            int transformedAlphabetIndex = (currentAlphabetIndex + 13) % LOWERCASE.length;
-            if (currentAlphabetIndex > 0) {
-                workingStringBuffer.append(LOWERCASE(currentAlphabetIndex));
-            } else { // només funciona si tots dos arrays són igual de llargs
-                workingStringBuffer.append(UPPERCASE(currentAlphabetIndex));
-            }
+    // si és minúscula retorna [0, index]
+    // si és majúscula retorna [1, index]
+    // si no hi és retorna [-1, index]
+    private static int[] isInSet(char inputChar) {
+        int index = findIndex(inputChar, LOWERCASE);
+        if (index >= 0) {
+            return new int[] { 0, index };
         }
-        return workingStringBuffer.toString();
+        index = findIndex(inputChar, UPPERCASE);
+        if (index >= 0) {
+            return new int[] { 1, index };
+        }
+        return new int[] { -1, index };
     }
 
-    // vull reescriure-ho amb Streams
-    String desxifraRot13(String encryptedText) {
-        StringBuffer workingStringBuffer = newStringBuffer();
-        for (int i = 0; i < encryptedText.length(); i++) {
-            char currentCharacter = inputText.charAt(i);
-            int currentAlphabetIndex = findIndex(currentCharacter, LOWERCASE);
-            if (currentAlphabetIndex > 0) {
+    private static String xifraRot13(String inputText) {
+        return inputText.chars() // Java no permet fer map sobre arrays o strings així que creem un stream d'ints
+                .mapToObj(intChar -> (char) intChar)
+                .map(character -> {
+                    int[] characterData = isInSet(character);
+                    if (characterData[0] == 0) {
+                        return LOWERCASE[(characterData[1] + 13) % LOWERCASE.length]; // per tal d'evitar consultar un
+                                                                                      // índex que estigui fora de rang,
+                                                                                      // ens basem en el residu de la
+                                                                                      // divisió
+                    } else if (characterData[0] == 1) {
+                        return UPPERCASE[(characterData[1] + 13) % UPPERCASE.length]; // per tal d'evitar consultar un
+                                                                                      // índex que estigui fora de rang,
+                                                                                      // ens basem en el residu de la
+                                                                                      // divisió
+                    } else {
+                        return character; // si no és ni a LOWERCASE ni a UPPERCASE el retornem tal qual
+                    }
+                })
+                .map(character -> character.toString()) // convertim cada char en String i els unim amb
+                                                        // Collectors.joining()
+                .collect(Collectors.joining());
+    }
 
-            }
-        }
+    private static String desxifraRot13(String inputText) {
+        return inputText.chars() // Java no permet fer map sobre arrays o strings així que creem un stream d'ints
+                .mapToObj(intChar -> (char) intChar)
+                .map(character -> {
+                    int[] characterData = isInSet(character);
+                    if (characterData[0] == 0) {
+                        return LOWERCASE[Math.floorMod(characterData[1] - 13, LOWERCASE.length)]; // floorMod per a
+                                                                                                  // evitar sortir de
+                                                                                                  // rang en casos
+                                                                                                  // negatius
+                    } else if (characterData[0] == 1) {
+                        return UPPERCASE[Math.floorMod(characterData[1] - 13, UPPERCASE.length)]; // floorMod per a
+                                                                                                  // evitar sortir de
+                                                                                                  // rang en casos
+                                                                                                  // negatius
+                    } else {
+                        return character; // si no és ni a LOWERCASE ni a UPPERCASE el retornem tal qual
+                    }
+                })
+                .map(character -> character.toString()) // convertim cada char en String i els unim amb
+                                                        // Collectors.joining()
+                .collect(Collectors.joining());
     }
 
     public static void main(String[] args) {
+        String happyPath = "Nos Atacan Con Carbunco";
+        String happyPathEncoded = xifraRot13(happyPath);
+        String happyPathDecoded = desxifraRot13(happyPathEncoded);
+        System.out.println("string feliç: " + happyPath);
+        System.out.println("string feliç xifrat: " + happyPathEncoded);
+        System.out.println("string feliç desxifrat: " + happyPathDecoded);
 
+        String trickyPath = "çÇñÑ666 s̴t̴r̷i̵n̵g̷ ̵s̸a̵t̷à̴n̶i̵c̸";
+        String trickyPathEncoded = xifraRot13(trickyPath);
+        String trickyPathDecoded = desxifraRot13(trickyPathEncoded);
+        System.out.println("string punyeter: " + trickyPath);
+        System.out.println("string punyeter xifrat: " + trickyPathEncoded);
+        System.out.println("string punyeter desxifrat: " + trickyPathDecoded);
     }
 }
